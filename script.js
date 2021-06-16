32ea2aa5dfb4997231b7adf5260d34bf1267ca49
Dmitry <dbk76@mail.ru>
2021-06-16 18:29:54 +0500

    Циклы и ифы

--- script.js
+++ script.js
@@ -8,14 +8,39 @@ let appData = {
     timeData: time,
     savings: false
 };
-let a1 = prompt("Your required expense in this month?", ""),
-    a2 = prompt("How much is it?", ""),
-    a3 = prompt("Your required expense in this month?", ""),
-    a4 = prompt("How much is it?", "");
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-alert(appData.budget / 30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Your required expense in this month?", ""),
+        b = prompt ("How much is it?", "");
 
-alert(a1 + "  " + appData.expenses.a1);
-alert(a3 + "  " + appData.expenses.a3);
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
+};
+
+
+
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}
